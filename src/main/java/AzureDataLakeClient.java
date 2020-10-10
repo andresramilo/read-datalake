d641b2847cc775a55a504910046c13e7b33998f6
Andres Romero <andresramilo@gmail.com>
2020-10-09 22:25:28 -0300

    commit num2

---
+++ src/main/java/AzureDataLakeClient.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import com.azure.storage.common.StorageSharedKeyCredential;
+import com.azure.storage.file.datalake.DataLakeFileClient;
+import com.azure.storage.file.datalake.DataLakeFileSystemClient;
+import com.azure.storage.file.datalake.DataLakeServiceClient;
+import com.azure.storage.file.datalake.DataLakeServiceClientBuilder;
+
+
+public class AzureDataLakeClient {
+
+	private static final String accountName ="datalaketestintelie";
+	private static final String accountKey =
+	"nuBaXNp4H9GyGsSHd0DrO8pBPwp4o+7X3ThtLa0jp4G02m4diQ2QWvgsbEyxSeOYh7dRcbnRjrJiEnxT1aKviQ==";
+	private static final String fileSystemName = "datainput";
+	
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		DataLakeServiceClient cli = GetDataLakeServiceClient(accountName,accountKey);
+		DataLakeFileSystemClient fileSystemClient = cli.getFileSystemClient(fileSystemName);
+		DownloadFile2(fileSystemClient);
+	}
+	
+	
+	static public DataLakeServiceClient GetDataLakeServiceClient
+	(String accountName, String accountKey){
+
+	    StorageSharedKeyCredential sharedKeyCredential =
+	        new StorageSharedKeyCredential(accountName, accountKey);
+
+	    DataLakeServiceClientBuilder builder = new DataLakeServiceClientBuilder();
+	    builder.credential(sharedKeyCredential);
+	    builder.endpoint("https://" + accountName + ".dfs.core.windows.net");
+
+	    return builder.buildClient();
+	}
+	
+	static public void DownloadFile(DataLakeFileSystemClient fileSystemClient)
+		    throws FileNotFoundException, java.io.IOException{
+
+		    DataLakeFileClient fileClient = 
+		    		fileSystemClient.getFileClient("teste.txt");
+			    ByteArrayOutputStream os = new ByteArrayOutputStream();
+			    fileClient.read(os);
+			    String out = new String(os.toByteArray(), "UTF-8");
+			    System.out.println(out);
+			    os.close();
+		      
+	}
+	
+	static public void DownloadFile2(DataLakeFileSystemClient fileSystemClient)
+		    throws FileNotFoundException, java.io.IOException{
+
+		    DataLakeFileClient fileClient = 
+		    		fileSystemClient.getFileClient("teste.txt");
+		    String expression = "SELECT * from BlobStorage";
+		    InputStream is = fileClient.openQueryInputStream(expression);
+		    String result = new BufferedReader(new InputStreamReader(is))
+		    		  .lines().collect(Collectors.joining("\n"));
+		    System.out.println(result);
+		    
+		    
+		    
+		      
+	}
+	
+	
+	
+
+}
